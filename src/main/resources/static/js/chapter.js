9b17afa69820677a267506d9b6f11e01fb242ab4
tamguo <smiletocandy@qq.com>
2018-03-09 11:03:41 +0800

    chapter-章节功能

--- src/main/resources/static/js/chapter.js
+++ src/main/resources/static/js/chapter.js
@@ -18,4 +18,12 @@ $(function(){
 		$(this).find(".view-analyse").find(".view-link").css("color","#11a68d!important");
 		event.stopPropagation();
 	});
+	
+	// 章节
+	$(".content-wp .list-left .out-chapter").bind("click",function(event){
+		$(".content-wp .list-left .out-chapter").removeClass("show");
+		$(this).addClass("show");
+		$(".content-wp .list-right .detail-chapter").css("display","none");
+		$(".content-wp .list-right .detail-chapter[data-chapterid='"+$(this).attr("data-chapterId")+"']").css("display","block");
+	});
 })
