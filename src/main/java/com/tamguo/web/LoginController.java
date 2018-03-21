ce52c21e93ed9e28e27f376caf40c4e75a6de8be
tamguo <smiletocandy@qq.com>
2018-03-21 15:58:01 +0800

    会员中心-登录

--- src/main/java/com/tamguo/web/LoginController.java
+++ src/main/java/com/tamguo/web/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tamguo.model.MemberEntity;
 import com.tamguo.service.IMemberService;
 import com.tamguo.util.Result;
 
@@ -21,6 +22,24 @@ public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public ModelAndView login(ModelAndView model){
 		model.setViewName("login");
+		model.addObject("isVerifyCode" , "0");
+		return model;
+	}
+	
+	@RequestMapping(value = "/submitLogin", method = RequestMethod.POST)
+	public ModelAndView submitLogin(String  username , String password , String verifyCode , ModelAndView model , HttpSession session){
+		Result result = iMemberService.login(username, password , verifyCode);
+		if(result.getCode() == 200){
+			session.setAttribute("currMember", result.getResult());
+			model.setViewName("index");	
+		}else{
+			model.setViewName("login");	
+			model.addObject("code", result.getCode());
+			model.addObject("msg" , result.getMessage());
+			model.addObject("isVerifyCode" , ((MemberEntity)result.getResult()).getLoginFailureCount() >=3 ? "1" : "0");
+			model.addObject("username", username);
+		}
+		
 		return model;
 	}
 	
---
+++ src/main/resources/static/js/member/login.js
@@ -0,0 +1,52 @@
+$(function(){
+	
+	$("#TANGRAM__PSP_3__userName").bind("focus",function(){
+		$("#TANGRAM__PSP_3__userName").addClass("pass-text-input-focus");
+	}).bind("keyup",function(){
+		if($("#TANGRAM__PSP_3__userName").val() != ""){
+			$("#TANGRAM__PSP_3__userName_clearbtn").show();
+		}else{
+			$("#TANGRAM__PSP_3__userName_clearbtn").hide();
+		}
+	}).bind("blur",function(){
+		$("#TANGRAM__PSP_3__userName").removeClass("pass-text-input-focus");
+	});
+	
+	$("#TANGRAM__PSP_3__userName_clearbtn").bind("click",function(){
+		$("#TANGRAM__PSP_3__userName").val("");
+		$("#TANGRAM__PSP_3__userName_clearbtn").hide();
+		$("#TANGRAM__PSP_3__userName").focus();
+	});
+	
+	$("#TANGRAM__PSP_3__password").bind("focus",function(){
+		$("#TANGRAM__PSP_3__password").addClass("pass-text-input-focus");
+	}).bind("blur",function(){
+		$("#TANGRAM__PSP_3__password").removeClass("pass-text-input-focus");
+	}).bind("keyup",function(){
+		if($("#TANGRAM__PSP_3__password").val() != ""){
+			$("#TANGRAM__PSP_3__password_clearbtn").show();
+		}else{
+			$("#TANGRAM__PSP_3__password_clearbtn").hide();
+		}
+	});
+	
+	$("#TANGRAM__PSP_3__password_clearbtn").bind("click",function(){
+		$("#TANGRAM__PSP_3__password").val("");
+		$("#TANGRAM__PSP_3__password_clearbtn").hide();
+		$("#TANGRAM__PSP_3__password").focus();
+	});
+	
+	$("#TANGRAM__PSP_3__submit").bind("click",function(){
+		if($("#TANGRAM__PSP_3__userName").val() == ""){
+			$("#TANGRAM__PSP_3__userName").focus();
+			$("#TANGRAM__PSP_3__error").text("请您输入手机/邮箱/用户名");
+			return false;
+		}
+		if($("#TANGRAM__PSP_3__password").val() == ""){
+			$("#TANGRAM__PSP_3__password").focus();
+			$("#TANGRAM__PSP_3__error").text("请您输入密码");
+			return false;
+		}
+		return true;
+	});
+})
