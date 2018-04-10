638f88e4a906e65cd5634462b1569870c1694302
tamguo <smiletocandy@qq.com>
2018-04-10 15:57:39 +0800

    member-tamguo

---
+++ src/main/java/com/tamguo/web/member/AccountController.java
@@ -0,0 +1,17 @@
+package com.tamguo.web.member;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AccountController {
+
+	@RequestMapping(value = "/member/account", method = RequestMethod.GET)
+	public ModelAndView index(ModelAndView model){
+		model.setViewName("member/account");
+		return model;
+	}
+	
+}
